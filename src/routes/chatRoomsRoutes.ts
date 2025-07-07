4e254d85ec88bf69751b4d1e4200cad23f171ce0
Alex Romeu <alexxromeu@gmail.com>
2025-07-07 11:07:17 -0500

    7/7/2025 meaningless bugs

--- src/routes/chatRoomsRoutes.ts
+++ src/routes/chatRoomsRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { getChatRooms, createChatRoom } from "../controllers/chatRoomController"
 import { authenticate } from '../middleware/authenticate';
-import { registerChatSocket } from '../sockets/registerChatSocket'
+
 
 const router = Router();
 
 router.get("/chatrooms", authenticate, getChatRooms);
-router.post("/chatrooms", authenticate, registerChatSocket);
+router.post("/chatrooms", authenticate, createChatRoom);
 
 export default router;
