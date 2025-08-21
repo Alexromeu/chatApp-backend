854ca3c863463be6a1e879fbd8030ff7d9e9f07b
Alexromeu <96091554+Alexromeu@users.noreply.github.com>
2025-08-21 16:02:34 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -28,10 +28,10 @@ app.use(cors({
 }));
 
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "distro")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "distro", "main.tsx"));
 });
 
 
