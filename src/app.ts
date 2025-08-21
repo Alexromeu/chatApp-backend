23197af34e6642798250508fbc15ffc5c314177b
Alexromeu <96091554+Alexromeu@users.noreply.github.com>
2025-08-21 16:10:41 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use(cors({
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "main.html"));
 });
 
 
